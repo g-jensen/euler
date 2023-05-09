54f28aeb7dc045f00b239ec0534a363def3db650
g-jensen <greg.micah.jensen@gmail.com>
2023-05-09 18:50:49 -0400

    Solved #1

--- spec/euler/level1/problem001_spec.clj
+++ spec/euler/level1/problem001_spec.clj
@@ -7,9 +7,18 @@
 
 (describe "Euler Problem #1"
 
+  (it "Checks if a number is a multiple of 3 or 5"
+    (should (multiple-of-3-or-5? 0))
+    (should-not (multiple-of-3-or-5? 1))
+    (should-not (multiple-of-3-or-5? 2))
+    (should (multiple-of-3-or-5? 3))
+    (should-not (multiple-of-3-or-5? 4))
+    (should (multiple-of-3-or-5? 5))
+    (should (multiple-of-3-or-5? 6))
+    (should (multiple-of-3-or-5? 15)))
+
   (it "Solves #1"
     (should= 23 (euler-1 10))
-	(should= -1 (euler-1 1000)))
-  )
+	  (should= 233168 (euler-1 1000))))
 
 (run-specs)
--- src/euler/level1/problem001.clj
+++ src/euler/level1/problem001.clj
@@ -1,4 +1,13 @@
 (ns euler.level1.problem001)
 
+(defn multiple-of-3-or-5? [n]
+  (or (= 0 (mod n 3))
+      (= 0 (mod n 5))))
+
 (defn euler-1 [n]
-  )
+  (loop [m 1 acc 0]
+    (if (< m n)
+      (if (multiple-of-3-or-5? m)
+        (recur (inc m) (+ acc m))
+        (recur (inc m) acc))
+      acc)))
