90ad485b9eb0697dbcee1b27cac2c11400d8be65
g-jensen <greg.micah.jensen@gmail.com>
2023-05-17 21:20:17 -0400

    retried 7

--- spec/euler/level1/problem007_spec.clj
+++ spec/euler/level1/problem007_spec.clj
@@ -7,12 +7,33 @@
 
 (describe "Euler Problem #7"
 
-  (it "generates prime numbers"
-    (should= [2 3 5 7 11] (primes 5))
-    (should= [2 3 5 7 11] (take 5 (lazy-primes-overflows)))
-    (should= [2 3 5 7 11] (take 5 (lazy-primes))))
+  (context "Prime Checker"
+
+    (it "determines 1 to be non-prime"
+      (should-not (prime? 1)))
+
+    (it "determines 2 to be prime"
+      (should (prime? 2)))
+
+    (it "determines 4 to be non-prime"
+      (should-not (prime? 4)))
+
+    (it "determines 5 to be prime"
+      (should (prime? 5)))
+
+    (it "determines 6 to be non-prime"
+      (should-not (prime? 6)))
+
+    (it "determines 9 to be non-prime"
+      (should-not (prime? 9)))
+
+    (it "determines 15 to be non-prime"
+      (should-not (prime? 15))))
 
   (it "Solves #7"
+    (should= 2 (euler-7 1))
+    (should= 3 (euler-7 2))
+    (should= 5 (euler-7 3))
     (should= 13 (euler-7 6))
     (should= 104743 (euler-7 10001))))
 
--- src/euler/level1/problem007.clj
+++ src/euler/level1/problem007.clj
@@ -1,39 +1,17 @@
 (ns euler.level1.problem007)
 
+(defn factor-of? [n m]
+  (zero? (mod m n)))
+
 (defn prime? [n]
   (cond
     (<= n 1) false
-    (<= n 3) true
-    (zero? (mod n 2)) false
-    (zero? (mod n 3)) false
-    :else (every? #(and (not= 0 (mod n %))
-                        (not= 0 (mod n (+ 2 %))))
-                  (range 5 (inc (Math/sqrt n)) 6))))
-
-(defn primes [n]
-  (loop [m 2 primes []]
-    (if (>= (count primes) n)
-      primes
-      (if (every? #(not= 0 (mod m %)) primes)
-        (recur (inc m) (conj primes m))
-        (recur (inc m) primes)))))
-
-(defn lazy-primes-overflows
-  ([] (lazy-primes-overflows (iterate inc 2)))
-  ([primes] (let [prime (first primes)
-                  filtered (filter #(not= 0 (mod % prime)) (rest primes))]
-              (cons prime (lazy-seq (lazy-primes-overflows filtered))))))
-
-(defn lazy-primes
-  ([] (lazy-primes 2 []))
-  ([m primes] (if (prime? m)
-                (cons m (lazy-seq (lazy-primes (inc m) (conj primes m))))
-                (lazy-primes (inc m) primes))))
+    (= n 2) true
+    :else (every? #(not (factor-of? % n)) (range 2 (inc (Math/sqrt n))))))
 
 (defn euler-7 [n]
-  #_(last (primes n))
-  #_(nth (lazy-primes) (dec n))
-  (loop [i 0 m 1]
-    (if (>= i n)
-      (dec m)
-      (recur (if (prime? m) (inc i) i) (inc m)))))
+  (loop [i 1 m 2]
+    (cond
+      (>= i n) m
+      (prime? (inc m)) (recur (inc i) (inc m))
+      :else (recur i (inc m)))))
