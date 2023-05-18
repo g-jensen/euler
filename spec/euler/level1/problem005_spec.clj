81364cb2bc45875a14819e9428e304d320dec131
g-jensen <greg.micah.jensen@gmail.com>
2023-05-17 21:18:55 -0400

    Renamed gcd and lcm to greatest-common-divisor and least-common-multiple respectively

--- spec/euler/level1/problem005_spec.clj
+++ spec/euler/level1/problem005_spec.clj
@@ -7,21 +7,23 @@
 
 (describe "Euler Problem #5"
 
-  (it "finds the gcd of two numbers"
-    (should= 1 (gcd 1 1))
-    (should= 2 (gcd 2 2))
-    (should= 2 (gcd 4 2))
-    (should= 48 (gcd (* 48 2) (* 48 3))))
+  (it "finds the greatest common divisor of two numbers"
+    (should= 1 (greatest-common-divisor 1 1))
+    (should= 2 (greatest-common-divisor 2 2))
+    (should= 2 (greatest-common-divisor 4 2))
+    (should= 48 (greatest-common-divisor (* 48 2) (* 48 3))))
 
-  (it "finds the lcm of two numbers"
-    (should= 6 (lcm 2 3))
-    (should= 8 (lcm 4 8))
-    (should= 60 (lcm 12 15)))
+  (it "finds the least common multiple of two numbers"
+    (should= 6 (least-common-multiple 2 3))
+    (should= 8 (least-common-multiple 4 8))
+    (should= 60 (least-common-multiple 12 15)))
 
-  (it "finds the lcm of a list of numbers"
-    (should= 300 (lcm [12 15 75])))
+  (it "finds the least common multiple of a list of numbers"
+    (should= 300 (least-common-multiple [12 15 75])))
 
-  (it "Solves #5"
+  (it "solves #5"
+    (should= 2 (euler-5 2))
+    (should= 6 (euler-5 3))
     (should= 2520 (euler-5 10))
     (should= 232792560 (euler-5 20))))
 
--- src/euler/level1/problem005.clj
+++ src/euler/level1/problem005.clj
@@ -1,14 +1,14 @@
 (ns euler.level1.problem005)
 
-(defn gcd [n m]
+(defn greatest-common-divisor [n m]
   (loop [n n m m]
     (if (zero? m)
       n
       (recur m (mod n m)))))
 
-(defn lcm
-  ([n m] (/ (* n m) (gcd n m)))
-  ([v] (reduce lcm v)))
+(defn least-common-multiple
+  ([n m] (/ (* n m) (greatest-common-divisor n m)))
+  ([v] (reduce least-common-multiple v)))
 
 (defn euler-5 [n]
-  (lcm (range 2 (inc n))))
+  (least-common-multiple (range 2 (inc n))))
